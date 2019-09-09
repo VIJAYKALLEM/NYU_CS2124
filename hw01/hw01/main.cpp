b224bb48c3e2066f232e8871c64f57133e5b9366
simransoin <simranksoin@gmail.com>
2019-09-09 18:57:34 -0400

    Use vector instead of extended string

--- hw01/hw01/main.cpp
+++ hw01/hw01/main.cpp
@@ -1,29 +1,33 @@
-// Simran Soin
-// CS UY 2124
-// 11 September 2019
-// Homework 1 -- Caesar Cypher
+/*
+Simran Soin
+CS UY 2124
+11 September 2019
+Homework 1 -- Caesar Cypher
+*/
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 void decrypt_line(string& line, int step) {
-    // line takes the line and step as input
+    // function takes the line and step as input
     // iterates through each character and decrypts it
     // returns the decrypted line
     for (size_t i=0; i< line.size(); ++i) {
         if (line[i] > 96 && line[i] < 123) {
+            // if char is out of lowercase alphabet ASCII range, it adds 26 to wrap around
             line[i] = line[i] - step;
             if (line[i] < 97){
-                line[i] = line[i] + 26;
+                line[i] += 26;
             }
         }
     }
 }
 
-string open_file_and_decrypt_file(ifstream& file) {
-    string decrypted_text;
+vector<string> open_file_and_decrypt_file(ifstream& file) {
+    vector<string> decrypted_text(4);
     string line;
     int step;
     // reads the first thing in the file and sets it as the int "step"
@@ -32,8 +36,7 @@ string open_file_and_decrypt_file(ifstream& file) {
         
         // for each line in the file, calls function decrypt_line on that string
         decrypt_line(line, step);
-        // adds line on TOP of the other decrypted text, to reverse the order of the lines.
-        decrypted_text = line + "\n" + decrypted_text;
+        decrypted_text.push_back(line);
     }
     file.close();
     return(decrypted_text);
@@ -45,7 +48,10 @@ int main() {
         cout << "Could not open the file";
     }
     else {
-        cout << open_file_and_decrypt_file(file) << endl;
+        vector<string> decrypted_file = open_file_and_decrypt_file(file);
+        for (size_t vector_size=decrypted_file.size() - 1; vector_size>0; --vector_size) {
+            cout << decrypted_file[vector_size] << endl;
+        }
     }
     return 0;
 }
